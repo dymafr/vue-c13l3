0c021da7e255980403366eca772f1a058276b6ec
dymafr <erwan1309@hotmail.com>
2022-03-21 17:59:37 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
-<template>
-  <div class="p-20">
-    <button @click="selectedComponant = 'PageA'" class="btn btn-danger mr-10">
-      Page A
-    </button>
-    <button @click="selectedComponant = 'PageB'" class="btn btn-primary mr-10">
-      Page B
-    </button>
-    <KeepAlive :max="5">
-      <Component :is="composants[selectedComponant]" />
-    </KeepAlive>
-  </div>
-</template>
+<template></template>
 
 <script setup lang="ts">
-import PageA from './PageA.vue';
-import PageB from './PageB.vue';
 import { ref, type Component as C } from 'vue';
-
-const composants: { [s:string]: C } = {
-  PageA,
-  PageB,
-};
-
-const selectedComponant = ref('PageA');
 </script>
 
 <style lang="scss">
