bdc34d666a58db2aa8fb7b1c870b2306778b1800
dymafr <erwan1309@hotmail.com>
2022-03-21 18:21:23 +0100

    1 file added

---
+++ src/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="open = true" class="btn btn-primary">
+    Confirmer l'achat
+  </button>
+  <div v-if="open" class="calc">
+    <div class="modal-container">
+      <h3>Confirmation de votre commande</h3>
+      <ul>
+        <li>Du contenu</li>
+        <li>Du contenu</li>
+        <li>Du contenu</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const open = ref(false);
+</script>
+
+<style lang="scss">
+@import './assets/scss/base.scss';
+</style>
